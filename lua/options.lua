3552d4482680a35df1022ddff1fd8969a3cb691c
Matheus Felipe <theusf40@hotmail.com>
2025-05-14 21:58:18 -0300

    Adicionado configurações de options

--- lua/options.lua
+++ lua/options.lua
@@ -2,4 +2,20 @@
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '>> ', trail = '·', nbsp = '_'}
+vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
+vim.opt.confirm = true
